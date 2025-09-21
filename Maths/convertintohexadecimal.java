373fc6254aa0af11c17d981ce8d2745c74ec474d
Gautam <pandeygautam394@gmail.com>
2025-09-21 08:18:56 +0530

    Covert integer to Hexadecimal:

--- Maths/convertintohexadecimal.java
+++ Maths/convertintohexadecimal.java
@@ -1,5 +1,17 @@
-public class convertintohexadecimal {
-    public static void main(String[] args) {
-        
-    }
+class Solution {
+    public String toHex(int num) {
+        if(num==0) return "0";
+        StringBuilder hex= new StringBuilder();
+        int rem;
+        while(num!=0) {
+            rem=num&15;
+            num>>>=4;
+            if(rem < 10) {
+                hex.insert(0,rem);
+            } else {
+                hex.insert(0,(char)(87+rem));
+            }
+        }
+        return hex.toString();
+    } 
 }
