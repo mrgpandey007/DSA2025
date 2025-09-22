52d023cec7c4a6fb6d95de588bbebc0f60f451cb
Gautam <pandeygautam394@gmail.com>
2025-09-22 07:26:33 +0530

    SQRT Completed 22092025072645:

---
+++ Maths/Sqrt.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x<2) return x;
+        int left=1,right=x/2,mid,ans=0;
+        long sq;
+        while(left<=right) {
+            mid=left+(right-left)/2;
+            sq=(long)mid*mid;
+            if(sq==x) return mid;
+            else if(sq<x) {
+                ans=mid;
+                left=mid+1;
+            } else {
+                right=mid-1;
+            }
+        }
+        return ans;
+    }
+}
