ac2bb5e6740b78da60ddd3613fbb1bbef1dd40f9
Gautam <pandeygautam394@gmail.com>
2025-09-23 06:55:42 +0530

    Add Binary:

---
+++ Maths/AddBinary.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder ans=new StringBuilder();
+        int i=a.length()-1,j=b.length()-1,sum=0;
+        while(i>=0||j>=0||sum>0) {
+            if(i>=0) sum += (a.charAt(i--)-'0');
+            if(j>=0) sum += (b.charAt(j--)-'0');
+            ans.append(sum%2);
+            sum/=2;
+        }
+        return ans.reverse().toString();
+    }
+}
